0506ee3703e9d737a4e81b2049b080f88eaee810
Mark Semeniuk <markflint2608@gmail.com>
2024-05-29 14:34:46 +0200

    First commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 mongoose
   .connect(
-    "mongodb+srv://haris2iftikhar:GClTzr15XhkjvN6k@backenddb.nrurtot.mongodb.net/Node-API?retryWrites=true&w=majority"
+    "mongodb+srv://Mark:1234@backenddb.xjrmgwu.mongodb.net/?retryWrites=true&w=majority&appName=backendDB"
   )
   .then(() => {
     console.log("Connected to database!");
